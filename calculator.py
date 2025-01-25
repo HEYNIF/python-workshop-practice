f7261fc166915177913ccc581d29c161f66dfa0b
HEYNIF <hanifshaikhdevil@gmail.com>
2025-01-25 17:33:47 +0300

    code updated

--- calculator.py
+++ calculator.py
@@ -2,4 +2,8 @@ num1 = int(input("enter the first no: "))
 num2 = int(input("enter the second no: "))
 
 sum_of_num = num1 + num2
-print(sum_of_num)
+print("sum of two number is ", sum_of_num)
+
+dif_of_num = num1 - num2
+
+print("dif of two number is", dif_of_num)
